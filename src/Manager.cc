85517e7fb74b0306867b271b1774da986402458d
Nate Koenig <nate@openrobotics.org>
2021-10-15 09:28:18 -0700

    Fixed build
    Signed-off-by: Nate Koenig <nate@openrobotics.org>

--- src/Manager.cc
+++ src/Manager.cc
@@ -709,10 +709,10 @@ void ManagerPrivate::ParseExecutables(const tinyxml2::XMLElement *_elem)
     {
       // Prepend an extra path for the ign binary in the case
       // of a Bazel build
+#ifdef BAZEL_IGN_PREFIX
       if (cmdParts[0] == "ign")
-      {
         cmdParts[0] = BAZEL_IGN_PREFIX + cmdParts[0];
-      }
+#endif
       if (!this->RunExecutable(nameStr, cmdParts, autoRestart, localEnvs))
       {
         ignerr << "Unable to run executable named[" << nameStr << "] in "
